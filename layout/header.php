95aaec3762868c795568657aa3824f6b8eaffaad
MaximSolodovnikov <MaximSolodovnikov@yandex.ua>
2015-09-11 11:30:02 +0300

    Added style.css and layout header of the site

---
+++ layout/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+	<head>
+		<title>Сайт на ООП</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<link type="text/css" rel="stylesheet" href="style/style.css" />
+	</head>
+	<body>
+		<div id="container">
+			
+			<div id="header"><h1>Header</h1></div>
