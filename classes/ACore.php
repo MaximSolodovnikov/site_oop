19a04c2a67a342c6c2d9cf0024c5e157d208bc1b
MaximSolodovnikov <MaximSolodovnikov@yandex.ua>
2015-09-11 10:31:26 +0300

    Added constructor in ACore.php which connects site with DB

--- classes/ACore.php
+++ classes/ACore.php
@@ -1,6 +1,26 @@
 <?php
 abstract class ACore {
 	
+	protected $db;
+	
+	public function __construct() {
+		
+		$this->db = mysql_connect(HOST, USER, PASS);
+		if (! $this->db) {
+			
+			exit("<br />Нет соединения с Базой данных. <br />" . mysql_error());
+			
+		}
+		if (! mysql_select_db(DB, $this->db)) {
+			
+			exit("<br />Не существует такой Базы данных. <br />" . mysql_error());
+			
+		}
+		
+		mysql_query("SET NAMES 'UTF-8'");
+		
+	}
+	
 	public function get_body() {
 		
 		echo "Hello, world";
