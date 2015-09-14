8b00c1278fe01330abf50613cf20dc612ba7a941
MaximSolodovnikov <MaximSolodovnikov@yandex.ua>
2015-09-14 17:25:20 +0300

    Finished the part of the get_content method in admin.php

--- classes/admin.php
+++ classes/admin.php
@@ -3,11 +3,24 @@ class admin extends ACore_admin {
 	
 	public function get_content() {
 		
-		echo "<div id='wrapper'>
-				<div id='content'>";
-				
-					
-		echo "</div>
-			</div>";
+		$query = "SELECT id_article, title_article FROM articles";
+		$result = mysql_query($query);
+		
+		if(! $result) {
+			
+			exit("<br />Ошибка выборки<br /> . mysql_error()");
+			
+		}
+		echo "<div id='wrapper'>";
+		
+		$row = array();
+		for($i = 0; $i < mysql_num_rows($result); $i++) {
+			
+			$row = mysql_fetch_array($result, MYSQL_ASSOC);
+			
+			printf("<div id='admin_articles'><a href='?option=update_articles&id_text=%s'>%s</a></div>",
+				$row['id_article'], $row['title_article']);
+		}		
+		echo "</div>";
 	}
 }
