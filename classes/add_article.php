aa62fca24656cf30f72071b5f1469b26a43b8006
MaximSolodovnikov <MaximSolodovnikov@yandex.ua>
2015-09-17 17:33:52 +0300

    Rewrited design of output articles in admin.php

--- classes/add_article.php
+++ classes/add_article.php
@@ -1,55 +1,56 @@
 <?php
 class add_article extends ACore_admin {
 	
-	protected function obr() {
-		
-		if(! empty($_FILES['img_src']['tmp_name'])) {
-			
-			$uploaddir = 'img/';
-			if(! move_uploaded_file($_FILES['img_src']['tmp_name'], 
-									$uploaddir . $_FILES['img_src']['name'])) {
-				exit("Не удалось загрузить изображение.");				
-			}
-			$img_src = $uploaddir . $_FILES['img_src']['name'];
-		}
-		
-		$title = $_POST['title'];
-		$description = $_POST['description'];
-		$text = $_POST['text'];
-		$date = $_POST['date'];
-		$cat = $_POST['cat'];
-		
-		if(! empty($title) || ! empty($description) || ! empty($text)){
-			
-			$query = "INSERT INTO articles (title_article, img_src, description_article, text_article, date, cat)
-					  VALUES('$title','$img_src', '$description', '$text', '$date', '$cat')";
-			if(! mysql_query($query)) {
-				
-				exit("<br />Ошибка внесения в БД<br />" . mysql_error());
-				
-			}
-			else {
-			
-				$_SESSION['res'] = "Данные успешно внесены в БД";
-				header("Location:?option=add_article");
-				exit();
-			}
-		}
-		else {
-			exit("<br />Вы не заполнили все поля.<br />");
-		}
-	}
-	
-	public function get_content() {
-		
-		echo "<div id='wrapper'>";
-		
-		if($_SESSION['res']) {
-			
-			echo $_SESSION['res'];
-			unset($_SESSION['res']);
-		}
-		$cat = $this->get_categories();
+    protected function obr() {
+
+        if(! empty($_FILES['img_src']['tmp_name'])) {
+
+                $uploaddir = 'img/';
+                if(! move_uploaded_file($_FILES['img_src']['tmp_name'], 
+                                                                $uploaddir . $_FILES['img_src']['name'])) {
+                        exit("Не удалось загрузить изображение.");				
+                }
+                $img_src = $uploaddir . $_FILES['img_src']['name'];
+        }
+
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $text = $_POST['text'];
+        $date = $_POST['date'];
+        $cat = $_POST['cat'];
+
+        if(! empty($title) || ! empty($description) || ! empty($text)){
+
+            $query = "INSERT INTO articles (title_article, img_src, description_article, text_article, date, cat)
+                                      VALUES('$title','$img_src', '$description', '$text', '$date', '$cat')";
+                if(! mysql_query($query)) {
+
+                        exit("<br />Ошибка внесения в БД<br />" . mysql_error());
+
+                }
+                else {
+
+                    $_SESSION['res'] = "Данные успешно внесены в БД";
+                    header("Location:?option=add_article");
+                    exit();
+                }
+            }
+            else {
+                exit("<br />Вы не заполнили все поля.<br />");
+            }
+    }
+
+    public function get_content() {
+
+        echo "<div id='wrapper'>";
+
+        if($_SESSION['res']) {
+
+            echo $_SESSION['res'];
+            unset($_SESSION['res']);
+        }
+        
+        $cat = $this->get_categories();
 				
 print <<<HEREDOC
 <form enctype='multipart/form-data' action='' method='POST'>
--- classes/admin.php
+++ classes/admin.php
@@ -1,32 +1,33 @@
 <?php
 class admin extends ACore_admin {
 	
-	public function get_content() {
-		
-		$query = "SELECT id_article, title_article FROM articles";
-		$result = mysql_query($query);
-		
-		if(! $result) {
-			
-			exit("<br />Ошибка выборки<br />" . mysql_error());
-			
-		}
-		echo "<div id='wrapper'>";
-		echo "<a href='?option=add_article'>Добавление статьи.<a/><hr />";
-		
-		
-		$row = array();
-		for($i = 0; $i < mysql_num_rows($result); $i++) {
-			
-			$row = mysql_fetch_array($result, MYSQL_ASSOC);
-			
-			
-			printf("<span id='admin_articles'>
-						Статья: <a href='?option=update_article&id_text=%s'>%s</a>
-						<a href='?option=delete_article&id_text=%s'>Удалить статью.</a><hr />
-					</span>",
-				$row['id_article'], $row['title_article'], $row['id_article']);
-		}		
-		echo "</div>";
-	}
+    public function get_content() {
+
+        $query = "SELECT id_article, title_article FROM articles";
+        $result = mysql_query($query);
+
+        if(! $result) {
+
+                exit("<br />Ошибка выборки<br />" . mysql_error());
+
+        }
+        echo "<div id='wrapper'>";
+        echo "<a href='?option=add_article'>Добавление статьи.<a/><hr />";
+
+        $row = array();
+        for($i = 0; $i < mysql_num_rows($result); $i++) {
+
+                $row = mysql_fetch_array($result, MYSQL_ASSOC);?>
+
+                    <h2>Статья: <?php echo $row['title_article'];?></h2>
+                    <a href='?option=update_article&id_text=<?php echo $row['id_article'];?>'>
+                        Редактировать статью.
+                    </a>
+                    <a href='?option=delete_article&id_text=<?php echo $row['id_article'];?>'>
+                        Удалить статью.
+                    </a>
+                    <hr />
+        <?php }		
+        echo "</div>";
+    }
 }
